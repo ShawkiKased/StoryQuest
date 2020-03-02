29c0afa5fe3aadc02dd2370fea2601a2f2fd0f12
Shawki <shawksta64@gmail.com>
2020-03-01 23:40:57 -0500

    Updated with real name

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles/main.css";
+
+const testObj = {
+  key1: "hi",
+  key2: {
+    key3: "Hello"
+  }
+};
+
+const greeting = testObj?.key2?.key3 || testObj.key1;
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("center");
+  const card = document.createElement("div");
+  card.classList.add("card", "center");
+  card.innerHTML = `<h2>${greeting} World!</h2>`;
+  document.body.append(card);
+  const imgCard = document.createElement("div");
+  imgCard.classList.add("card", "center", "image-card");
+  document.body.appendChild(imgCard);
+});
