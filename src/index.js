c675c4a14f4dc5ef1d2ddbba19a23860338ee7a5
Shawki <shawksta64@gmail.com>
2020-03-02 18:26:45 -0500

    Practicing canvus and stretching an area for the map

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import "./styles/index.scss";
+// import "./styles/index.scss";
 
-const testObj = {
-  key1: "hi",
-  key2: {
-    key3: "Hello"
-  }
-};
+// const testObj = {
+//   key1: "hi",
+//   key2: {
+//     key3: "Hello"
+//   }
+// };
 
-const greeting = testObj?.key2?.key3 || testObj.key1;
-window.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.add("center");
-  const card = document.createElement("div");
-  card.classList.add("card", "center");
-  card.innerHTML = `<h2>${greeting} World!</h2>`;
-  document.body.append(card);
-  const imgCard = document.createElement("div");
-  imgCard.classList.add("card", "center", "image-card");
-  document.body.appendChild(imgCard);
-});
+// const greeting = testObj?.key2?.key3 || testObj.key1;
+// window.addEventListener("DOMContentLoaded", () => {
+//   document.body.classList.add("center");
+//   const card = document.createElement("div");
+//   card.classList.add("card", "center");
+//   card.innerHTML = `<h2>${greeting} World!</h2>`;
+//   document.body.append(card);
+//   const imgCard = document.createElement("div");
+//   imgCard.classList.add("card", "center", "image-card");
+//   document.body.appendChild(imgCard);
+// });
