9cd649e04658a5b26ad61e4b650d025de41dc175
Shawki <shawksta64@gmail.com>
2020-03-03 18:07:32 -0500

    played around with canvus, going for DOM Manipulation, got background image example

--- src/index.js
+++ src/index.js
@@ -1,20 +1,173 @@
-// import "./styles/index.scss";
+import "./styles/index.scss";
 
-// const testObj = {
-//   key1: "hi",
-//   key2: {
-//     key3: "Hello"
-//   }
-// };
+const testObj = {
+  key1: "hi",
+  key2: {
+    key3: "Hello"
+  }
+};
 
-// const greeting = testObj?.key2?.key3 || testObj.key1;
-// window.addEventListener("DOMContentLoaded", () => {
-//   document.body.classList.add("center");
-//   const card = document.createElement("div");
-//   card.classList.add("card", "center");
-//   card.innerHTML = `<h2>${greeting} World!</h2>`;
-//   document.body.append(card);
-//   const imgCard = document.createElement("div");
-//   imgCard.classList.add("card", "center", "image-card");
-//   document.body.appendChild(imgCard);
-// });
+const greeting = testObj?.key2?.key3 || testObj.key1;
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("center");
+  const mainGame = document.createElement("div");
+  mainGame.classList.add("center", "main-game");
+  document.body.appendChild(mainGame);
+});
+
+// <!DOCTYPE html>
+// <html>
+//   <head>
+//       <meta charset="utf-8" />
+//       <title>Gamedev Canvas Workshop</title>
+//       <style>
+//         * { padding: 0; margin: 0; }
+//     	  canvas { background: #eee; display: block; margin: 0 auto; }
+//       </style>
+//   </head>
+//   <body>
+    
+//     <canvas id="myCanvas" width="1000" height="800"></canvas>
+    
+//     <script>
+//       let canvas = document.getElementById("myCanvas");
+//       let ctx = canvas.getContext("2d");
+//       let ballRadius = 10;
+//       let x = canvas.width / 2;
+//       let y = canvas.height - 30;
+//       let paddleHeight = 10;
+//       let paddleWidth = 150;
+//       let paddleX = (canvas.width - paddleWidth) / 2;
+//       let rightPressed = false;
+//       let leftPressed = false;
+//       let brickRowCount = 3;
+//       let brickColumnCount = 11;
+//       let brickWidth = 75;
+//       let brickHeight = 20;
+//       let brickPadding = 10;
+//       let brickOffsetTop = 30;
+//       let brickOffsetLeft = 30;
+//       let bricks = [];
+//       let dx = 2;
+//       let dy = -2;
+//       let colors = ["red", "green", "blue", "yellow", "purple", "pink", "white", "black"];
+//       let color = "#0095DD";
+      
+//       for (let c = 0; c < brickColumnCount; c++) {
+//         bricks[c] = [];
+//         for (let r = 0; r<brickRowCount; r++) {
+//           bricks[c][r] = { x: 0, y: 0 };
+//         }
+//       }
+
+//       function drawBricks() {
+//         for (let c = 0; c < brickColumnCount; c++) {
+//           for (let r = 0; r < brickRowCount; r++) {
+//             let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+//             let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+//             bricks[c][r].x = brickX;
+//             bricks[c][r].y = brickY;
+//             ctx.beginPath();
+//             ctx.rect(brickX, brickY,  brickWidth, brickHeight);
+//             ctx.fillStyle = "#0095DD";
+//             ctx.fill();
+//             ctx.closePath();
+//           }
+//         }
+//       }
+
+//       function drawBall() {
+//         ctx.beginPath();
+//         ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+//         ctx.fillStyle = color;
+//         ctx.fill();
+//         ctx.closePath();
+//       }
+
+//      function drawPaddle() {
+//         ctx.beginPath();
+//         ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+//         ctx.fillStyle = "#0095DD";
+//         ctx.fill();
+//         ctx.closePath();
+//       }
+
+//       function draw() {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//         drawBricks();
+//         drawBall();
+//         drawPaddle();
+
+//         if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+//           color = colors[Math.ceil(Math.random() * 8)];
+//           dx = -dx;
+//         }
+//         if (y + dy < ballRadius) {
+//           color = colors[Math.ceil(Math.random() * 8)];
+//           dy = -dy;
+//         } else if(y + dy > canvas.height-ballRadius) {
+//           if(x > paddleX && x < paddleX + paddleWidth) {
+//             color = colors[Math.ceil(Math.random() * 8)];
+//             dy += 1;
+//             dy = -dy;
+
+//           } else {
+//             alert("GAME OVER");
+//             document.location.reload();
+//             clearInterval(interval);
+//           }
+//         }
+//         if (rightPressed) {
+//           paddleX += 7;
+//           if (paddleX + paddleWidth > canvas.width) {
+//             paddleX = canvas.width - paddleWidth;
+//           }
+//         }
+//         else if (leftPressed) {
+//           paddleX -= 7;
+//           if (paddleX < 0) {
+//             paddleX = 0;
+//           }
+//         }
+
+//         x += dx;
+//         y += dy;
+//       }
+
+//       let interval = setInterval(draw, 10);
+//       document.addEventListener("keydown", keyDownHandler, false);
+//       document.addEventListener("keyup", keyUpHandler, false);
+
+//       function keyDownHandler(e) {
+//         if (e.key == "Right" || e.key == "ArrowRight") {
+//           rightPressed = true;
+//         }
+//         else if (e.key == "Left" || e.key == "ArrowLeft") {
+//           leftPressed = true;
+//         }
+//       }
+
+//       function keyUpHandler(e) {
+//         if (e.key == "Right" || e.key == "ArrowRight") {
+//           rightPressed = false;
+//         }
+//         else if (e.key == "Left" || e.key == "ArrowLeft") {
+//           leftPressed = false;
+//         }
+//       }
+
+//       function collisionDetection() {
+//         for (let c = 0; c < brickColumnCount; c++) {
+//           for (let r = 0; r < brickRowCount; r++) {
+//             let b = bricks[c][r];
+//             if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+//               dy = -dy;
+//             }
+//           }
+//         }
+//       }
+
+//     </script>
+
+//   </body>
+// </html>
